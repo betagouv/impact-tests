ae069d048217fe91748e62f61bc0ab2d49e55beb
Florian Delezenne <del.florian@gmail.com>
2015-02-04 18:31:06 +0100

    Simulation des prestations impact

--- impact-tests-api/index.js
+++ impact-tests-api/index.js
@@ -3,6 +3,8 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var ludwigApi = require('ludwig-api');
+var Simulation = require('./simulation');
+var _ = require('lodash');
 
 // Setup mongoose
 mongoose.connect(process.env.MONGODB_URL);
@@ -13,86 +15,90 @@ var app = express();
 // Mount Ludwig API
 // var Situation = mongoose.model('Situation');
 
+var possibleValues = [
+    {
+        id: 'carteStationnement',
+        shortLabel: 'Carte stationnement',
+        title: 'Carte de stationnement',
+        hasMontant: false
+    },
+    {
+        id: 'carteInvalidite',
+        shortLabel: 'Carte d\'invalidité',
+        title: 'Carte d\'invalidité',
+        hasMontant: false
+    },
+    {
+        id: 'aeeh',
+        shortLabel: 'AEEH',
+        title: 'Allocation d\'éducation de l\'enfant handicapé',
+        hasMontant: false
+    },
+    {
+        id: 'aah',
+        shortLabel: 'AAH',
+        title: 'Allocation aux adultes handicapés',
+        hasMontant: false
+    },
+    {
+        id: 'pch',
+        shortLabel: 'PCH',
+        title: 'Prestation de compensation du handicap',
+        hasMontant: false
+    },
+    {
+        id: 'rqth',
+        shortLabel: 'RQTH',
+        title: 'Reconnaissance de la Qualité de Travailleur Handicapé',
+        hasMontant: false
+    },
+    {
+        id: 'av',
+        shortLabel: 'AV',
+        title: 'Affiliation gratuite à l\'assurance vieillesse',
+        hasMontant: false
+    },
+    {
+        id: 'ems',
+        shortLabel: 'EMS',
+        title: 'Accompagnement par un service ou établissement médico-social',
+        hasMontant: false
+    },
+    {
+        id: 'pps',
+        shortLabel: 'PPS',
+        title: 'Plan personnalisé de scolarisation',
+        hasMontant: false
+    },
+    {
+        id: 'orp',
+        shortLabel: 'ORP',
+        title: 'Orientation professionnelle',
+        hasMontant: false
+    },
+    {
+        id: 'ac',
+        type: 'presta-finances',
+        title: 'Allocation compensatrice',
+        hasMontant: false
+    }
+];
+
 app.use(ludwigApi({
 
     mongoose: mongoose,
 
-    possibleValues: [
-        {
-            id: 'carteStationnement',
-            shortLabel: 'Carte stationnement',
-            title: 'Carte de stationnement',
-            hasMontant: false
-        },
-        {
-            id: 'carteInvalidite',
-            shortLabel: 'Carte d\'invalidité',
-            title: 'Carte d\'invalidité',
-            hasMontant: false
-        },
-        {
-            id: 'aeeh',
-            shortLabel: 'AEEH',
-            title: 'Allocation d\'éducation de l\'enfant handicapé',
-            hasMontant: false
-        },
-        {
-            id: 'aah',
-            shortLabel: 'AAH',
-            title: 'Allocation aux adultes handicapés',
-            hasMontant: false
-        },
-        {
-            id: 'pch',
-            shortLabel: 'PCH',
-            title: 'Prestation de compensation du handicap',
-            hasMontant: false
-        },
-        {
-            id: 'rqth',
-            shortLabel: 'RQTH',
-            title: 'Reconnaissance de la Qualité de Travailleur Handicapé',
-            hasMontant: false
-        },
-        {
-            id: 'av',
-            shortLabel: 'AV',
-            title: 'Affiliation gratuite à l\'assurance vieillesse',
-            hasMontant: false
-        },
-        {
-            id: 'ems',
-            shortLabel: 'EMS',
-            title: 'Accompagnement par un service ou établissement médico-social',
-            hasMontant: false
-        },
-        {
-            id: 'pps',
-            shortLabel: 'PPS',
-            title: 'Plan personnalisé de scolarisation',
-            hasMontant: false
-        },
-        {
-            id: 'orp',
-            shortLabel: 'ORP',
-            title: 'Orientation professionnelle',
-            hasMontant: false
-        },
-        {
-            id: 'ac',
-            type: 'presta-finances',
-            title: 'Allocation compensatrice',
-            hasMontant: false
-        }
-    ],
+    possibleValues: possibleValues,
 
     simulate: function (acceptanceTest, done) {
-        // Situation.findById(acceptanceTest.scenario.situationId).exec(function (err, situation) {
-        //     if (err) return done(err);
-        //     if (!situation) return done(new Error('Situation not found'));
-        //     situation.simulate(done);
-        // });
-        done(null, {'pch': true});
+        var result = Simulation.compute(acceptanceTest.scenario, possibleValues);
+
+        var droitsObtenus = {};
+        _.forEach(result, function(droit) {
+            droitsObtenus[droit.id] = true;
+        });
+
+        done(null, droitsObtenus);
     },
 
     onCreate: function (acceptanceTest, done) {
